8f7d1c7e8092b79f098fa812d4776aa114480e82
ayushpantve <ayushpant@virtualemployee.com>
2019-03-15 22:04:52 +0530

    The new one with search

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = {
+    text: ' '
+  };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.text);
+  }
+  render() {
+    return (
+    <div className= "ui segment">
+      <form className="ui form" onSubmit={this.onFormSubmit}>
+      <div className= "field">
+        Image Search
+        <input type="text" 
+        value = { this.state.text }
+        onChange={ e => this.setState( { text: e.target.value } ) }></input>
+      { this.state.text }
+      </div> 
+      </form>
+    </div>
+    )
+  }
+
+}
+
+export default SearchBar;
